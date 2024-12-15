04b829396db7061e129ce63a0ddfd3ed7d448d3d
shyjek123 <sebby.hyjek@gmail.com>
2024-12-14 21:03:59 -0500

    feat: added the other basic config plugins, formatting, common repos, telescope, todo-cooments, treesitter, which-key

---
+++ nvim/lua/sebastian/plugins/formatting.lua
@@ -0,0 +1,30 @@
+-- defines formatters based on language
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        json = { "prettier" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+-- no sure what this does
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
