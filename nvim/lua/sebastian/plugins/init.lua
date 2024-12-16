9f317a35a4a78dad5e781d969b759bf93de7d9b0
shyjek123 <sebby.hyjek@gmail.com>
2024-12-16 12:52:18 -0500

    fix: refactored code and debugged telescope

--- nvim/lua/sebastian/plugins/init.lua
+++ nvim/lua/sebastian/plugins/init.lua
@@ -2,4 +2,5 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "junegunn/fzf",
 }
--- nvim/lua/sebastian/plugins/telescope.lua
+++ nvim/lua/sebastian/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-fzf-native.nvim" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
   },
@@ -36,15 +36,44 @@ return {
       },
     })
 
-    telescope.load_extension("fzf")
+    -- Enable Telescope extensions if they are installed
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "ui-select")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
+    local builtin = require("telescope.builtin")
+    keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+    keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+    keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+    keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+    keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+    -- Slightly advanced example of overriding default behavior and theme
+    keymap.set("n", "<leader>/", function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+        winblend = 10,
+        previewer = false,
+      }))
+    end, { desc = "[/] Fuzzily search in current buffer" })
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    -- It's also possible to pass additional configuration options.
+    --  See `:help telescope.builtin.live_grep()` for information about particular keys
+    keymap.set("n", "<leader>s/", function()
+      builtin.live_grep({
+        grep_open_files = true,
+        prompt_title = "Live Grep in Open Files",
+      })
+    end, { desc = "[S]earch [/] in Open Files" })
+
+    -- Shortcut for searching your Neovim configuration files
+    keymap.set("n", "<leader>sn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[S]earch [N]eovim files" })
   end,
 }
