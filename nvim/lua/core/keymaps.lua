27db488d3ac4e256691ed22d98616378d231abf4
Sebastian Hyjek <sebby.hyjek@gmail.com>
2026-07-08 09:12:22 -0400

    altered configs, namely theme file

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -6,4 +6,4 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("i", "JK", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- unhighlight searches with escape key
-
+keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
--- nvim/lua/plugins/todo_comments.lua
+++ nvim/lua/plugins/todo_comments.lua
@@ -18,31 +18,93 @@ return {
     todo_comments.setup({
       keywords = {
         FIX = {
-          icon = " ", -- icon used for the sign, and in search results
-          color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FAILED", "FIXME", "BUG", "FIXIT", "ISSUE", "REMOVE", "ERROR" }, -- a set of other keywords that all map to this FIX keywords
-          -- signs = false, -- configure signs for some keywords individually
+          icon = " ",
+          color = "error",
+          alt = { "FAILED", "FIXME", "BUG", "FIXIT", "ISSUE", "REMOVE", "ERROR" },
         },
-        TODO = { icon = " ", color = "#04fbe3", alt = { "ADD", "PASSED", "PLAN" } },
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        INFO = { icon = " ", color = "hint", alt = { "" } },
-        NOTE = { icon = " ", color = "default", alt = { "" } },
-        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
 
-        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-        info = { "DiagnosticInfo", "#2563EB" },
-        hint = { "DiagnosticHint", "#10B981" },
-        default = { "Identifier", "#7C3AED" },
-        test = { "Identifier", "#FF00FF" },
+        TODO = {
+          icon = " ",
+          color = "info",
+          alt = { "ADD", "PLAN" },
+        },
+
+        DONE = {
+          icon = " ",
+          color = "hint",
+          alt = { "PASSED" },
+        },
+
+        HACK = {
+          icon = " ",
+          color = "warning",
+        },
+
+        WARN = {
+          icon = " ",
+          color = "warning",
+          alt = { "WARNING", "XXX" },
+        },
+
+        PERF = {
+          icon = " ",
+          color = "purple",
+          alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" },
+        },
+
+        INFO = {
+          icon = " ",
+          color = "info",
+        },
+
+        NOTE = {
+          icon = "󰎚 ",
+          color = "default",
+        },
+
+        TEST = {
+          icon = "⏲ ",
+          color = "test",
+          alt = { "TESTING", "PASSED", "FAILED" },
+        },
+      },
+
+      colors = {
+        error   = { "DiagnosticError", "ErrorMsg", "#fb4934" }, -- red
+        warning = { "DiagnosticWarn", "WarningMsg", "#fabd2f" }, -- yellow
+        info    = { "DiagnosticInfo", "#83a598" }, -- blue
+        hint    = { "DiagnosticHint", "#8ec07c" }, -- aqua/green
+        default = { "Identifier", "#928374" }, -- gray
+        test    = { "Identifier", "#d3869b" }, -- purple
+        purple  = { "Constant", "#d3869b" }, -- PERF
       },
     })
+    -- todo_comments.setup({
+    --
+    --
+    --   keywords = {
+    --     FIX = {
+    --       icon = " ", -- icon used for the sign, and in search results
+    --       color = "error", -- can be a hex color, or a named color (see below)
+    --       alt = { "FAILED", "FIXME", "BUG", "FIXIT", "ISSUE", "REMOVE", "ERROR" }, -- a set of other keywords that all map to this FIX keywords
+    --       -- signs = false, -- configure signs for some keywords individually
+    --     },
+    --     TODO = { icon = " ", color = "#04fbe3", alt = { "ADD", "PLAN" } },
+    --     DONE = { icon = " ", color = "#04fbe3", alt = { "PASSED" } },
+    --     HACK = { icon = " ", color = "warning" },
+    --     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+    --     PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    --     INFO = { icon = " ", color = "hint", alt = { "" } },
+    --     NOTE = { icon = " ", color = "default", alt = { "" } },
+    --     TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+    --
+    --     error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+    --     warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+    --     info = { "DiagnosticInfo", "#2563EB" },
+    --     hint = { "DiagnosticHint", "#10B981" },
+    --     default = { "Identifier", "#7C3AED" },
+    --     test = { "Identifier", "#FF00FF" },
+    --   },
+    -- })
   end,
-{
-  "folke/ts-comments.nvim",
-  event = "VeryLazy",
-  opts = {},
-}
 }
